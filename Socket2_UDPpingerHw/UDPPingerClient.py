9c610c7162be38cb368b5a64682b98bd8e949d36
Quangtri Thai <trithai45@yahoo.com>
2018-02-16 23:18:10 -0600

    finish client

--- Socket2_UDPpingerHw/UDPPingerClient.py
+++ Socket2_UDPpingerHw/UDPPingerClient.py
@@ -1,5 +1,21 @@
 # UDPPingerClient.py
 from socket import *
+import time
+
+serverIP = "127.0.0.1"
+serverPort = 12000
 
 #Create a UDP socket: Internet Protocol V4, Datagram
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+for k in range(1,11):
+    try:
+        sendTime = time.time()
+        clientMessage = "Ping " + str(k) + " " + str(sendTime)
+        clientSocket.sendto(clientMessage, (serverIP, serverPort))
+        clientSocket.settimeout(1)
+        serverMessage, address = clientSocket.recvfrom(1024)
+        print(serverMessage + " RTT:" + str(time.time() - sendTime))
+    except timeout:
+        print "Request timed out"
+        continue
